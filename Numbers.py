0ffa8834ae3f8ca485fb06396ac4620043e4ba9f
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-03-02 01:25:58 +0500

    first commit

---
+++ Numbers.py
@@ -0,0 +1,10 @@
+print(2**4)
+print(4%2)
+nyc_bal=188
+bal_pitt=247
+total_dist=nyc_bal+bal_pitt
+print(total_dist)
+
+
+float_num=6.666
+print(round(float_num,1))
---
+++ variable.py
@@ -0,0 +1,6 @@
+a=20
+print("you value is ",a)
+age=25
+name="taimour"
+is_logged=True
+print("your name is "+name+"your age is ",age)
