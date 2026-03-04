5e15a7cd333cee482eb50780febeeb4270547bdd
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-03-05 01:20:31 +0500

    thursday

--- practice.py
+++ practice.py
@@ -120,15 +120,15 @@
 
 #prime number checker
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num > 1:
-    for i in range(2, num):
-        if num % i == 0:
-            print("Its not a prime number")
-            break
-    else:
-        # This else belongs to the for loop!
-        print("Its a prime number")
-else:
-    print("Its not a prime number") 
+# if num > 1:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("Its not a prime number")
+#             break
+#     else:
+#         # This else belongs to the for loop!
+#         print("Its a prime number")
+# else:
+#     print("Its not a prime number") 
