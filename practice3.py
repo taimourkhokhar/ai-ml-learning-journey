50f9e7e90f1262be405fa9aa6ad4e0e2d716f75b
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-03-06 02:50:48 +0500

    friday

--- practice3.py
+++ practice3.py
@@ -32,20 +32,91 @@
 #Function that take a string as input and returns
 
 
-userInput=input("Enter a string")
-vowel_count=0
-consonants_count=0
+# userInput=input("Enter a string")
+# vowel_count=0
+# consonants_count=0
 
-def counter(a):
-  global vowel_count,consonants_count
-  for i in a:
-    if i in 'aeiou':
-     vowel_count+=1
-    else:
-      consonants_count+=1
+# def counter(a):
+#   global vowel_count,consonants_count
+#   for i in a:
+#     if i in 'aeiou':
+#      vowel_count+=1
+#     else:
+#       consonants_count+=1
 
 
-counter(userInput)
+# counter(userInput)
 
-print(f"Vowels: {vowel_count}")
-print(f"consonant:{consonants_count}")
+# print(f"Vowels: {vowel_count}")
+# print(f"consonant:{consonants_count}")
+
+
+# new = []
+
+# for i in range(1, 11):
+#     user = int(input(f"Enter number {i}: "))
+#     new.append(user)
+
+# max_val = new[0]
+# min_val = new[0]
+# evens = []
+
+# for i in new:
+#     if i > max_val:
+#         max_val = i
+    
+#     if i < min_val:
+#         min_val = i
+    
+#     if i % 2 == 0:
+#         evens.append(i)
+
+# print("-" * 20)
+# print(f"Even numbers found: {evens}")
+# print(f"The largest number is: {max_val}")
+# print(f"The smallest number is: {min_val}")
+
+
+
+#create a dictionay for a student with keys: name,rollno,marks (marks should be a list of 5 subjents)
+# student_data = {
+#   "name": "Taimour",
+#   "roll_no": 70147,
+#   "marks": {
+#     "physics": 77,
+#     "math": 88,
+#     "chemistry": 32,
+#     "biology": 54
+#   }
+# }
+
+# total = 0
+# average=0
+# for subject, score in student_data["marks"].items():
+#     print(f"{subject}: {score}")
+#     total += score
+#     average=total/4
+#     if average>60:
+#         print(f"Student name: {student_data['name']}")
+# print(f"Average Marks: {average}")
+# print(f"Total Marks: {total}")
+
+
+
+#program that two sets of number from user
+
+# set1={1,2,3,4,5,6}
+# set2={4,5,6,7,8,9,10}
+
+# ans_set3=set1.union(set2)
+
+# ans_set4=set1.intersection(set2)
+
+# ans_set5=set1.difference(set2)
+
+# print(ans_set5)
+# print(ans_set4)
+# print(ans_set3)
+
+
+
