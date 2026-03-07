aef699c2e4488be6e4cad1f1c75ed22689585bab
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-03-07 21:49:43 +0500

    saturday

---
+++ practice6.py
@@ -0,0 +1,32 @@
+# break=5
+
+# #it show invalid syntax
+
+# birth_year=2000
+# current_year=2026
+# age=current_year-birth_year
+# print(age)
+
+# first="Taimour"
+# middle="Iftikhar"
+# Last="Khokhar"
+# print(first+' '+ middle+' '+ Last)
+
+
+# field_len=92
+# field_wid=48.8
+# total_area=field_wid*field_len
+
+# print(total_area)
+
+# bought=9
+# price=1.49
+# shopKeeper=20
+# total=bought*price
+# remain=shopKeeper-total
+# print(remain)
+
+# length=5.5
+# area=length**2 # area of square is length power 2
+# cost=area*500
+# print("total cost for bathroom tiles replacement:",cost) # Ans: 151
