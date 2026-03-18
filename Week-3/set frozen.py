0d9481449de2ed711cbf1df32decf91d582f2a09
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-03-18 22:08:43 +0500

    Wednesday work

--- Week-3/set frozen.py
+++ Week-3/set frozen.py
@@ -15,14 +15,14 @@
 
 #basice operation like union intersetion difference like
 
-# set1={1,2,3,4,5}
-# set2={2,3}
+set1={1,2,3,4,5}
+set2={2,3}
 
-# set3=set1.union(set2)
+set3=set1.union(set2)
 
 
 
-# print(set3)
-
-# print(set1.intersection(set2))
-# print(set2.issubset(set1))
+print(set3)
+print(set1.difference(set2))
+print(set1.intersection(set2))
+print(set2.issubset(set1))
