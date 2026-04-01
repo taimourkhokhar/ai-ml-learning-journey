733fcfb89e8be2e1b32a711d4f53f66e818ef9f6
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-01 14:27:34 +0500

    numpy practice

--- Week-5/Numpy1.py
+++ Week-5/Numpy1.py
@@ -40,6 +40,54 @@ import numpy as np
 
 #3-D array
 
-arr=np.array([[[1,2,3,4],[1,2,3,3],[4,5,6,7]]])
-print(np.ndim(arr))
-print(arr)
+# arr=np.array([[[1,2,3,4],[1,2,3,3],[4,5,6,7]]])
+# print(np.ndim(arr))
+# print(arr)
+
+
+#array indexing
+
+# arr=np.array([1,2,3,4])
+# print(arr[0])
+# print(arr[0]+arr[1])
+
+
+
+#accessing element in 2-D array
+
+# arr=np.array([[1,2,3,4],[5,6,7,8]])
+
+# print(arr[0,3])
+
+
+#accessing element in 3-D array
+
+# arr=np.array([[[1,2,3,4],[5,6,7,8],[9,10,11,12]]])
+
+# print(arr,np.ndim(arr))
+
+
+# #Accessing 11 in 3-D array
+# print(arr[0,0,1])
+
+# arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+# print('Last element from 2nd dim: ', arr[1, -1])
+
+
+# arr=np.array([1,2,3,4,5,6])
+# print(arr[:4])
+
+# arr=np.array([1,2,3,4,5,6,7])
+# print(arr[::2])
+
+
+#slicing in 2-D array
+# arr=np.array([[1,2,3,4,5],[7,8,9,10,11]])
+# print(arr[0:1,1:4])
+
+
+#slicing in 3-D array
+
+arr=np.array([[[1,2,3,4,5],[7,8,9,10,11],[12,13,14,15,16]]])
+print(arr[0,0:3,2])
