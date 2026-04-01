5184ae5f76be76a7c837e641a68ddb2d3d2e4538
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-01 23:54:16 +0500

    week 5 work

---
+++ Week-5/Matrix.py
@@ -0,0 +1,65 @@
+#sales data fro quarter 1 matrix1
+import numpy as np
+
+# q1=np.array([
+#   [200,220,250],
+#   [150,180,210],
+#   [300,330,360]
+# ])
+# q2=np.array([
+#   [209,231,259],
+#   [155,192,222],
+#   [310,340,375]
+# ])
+
+# q3=(q2-q1)*100/q1
+# print(q3)
+
+
+# #figer out revernuew
+
+# prices=np.array([
+#   [10,12,11],
+#   [8,9,10],
+#   [15,16,17]
+# ])
+
+# q1_revenue=q1*prices
+
+# print(q1_revenue)
+# q1_dicount=q1_revenue*0.2
+
+# #for net revenue
+
+# net_reveneue=q1_revenue-q1_dicount
+
+# print(net_reveneue)
+
+
+# print(np.sum(q1_dicount))
+
+
+
+#dot product
+# features=np.array([
+#   [2000,3],
+#   [1800,2]
+# ])
+
+# weigts=np.array([150,50000])
+
+# final=np.dot(features,weigts)
+# print(final)
+
+# #cross product
+
+# c=np.array([1,2,3])
+# d=np.array([4,5,6])
+# ans=np.cross(c,d)
+# print(ans)
+
+
+#slicing
+
+a=np.array([6,7,8,9])
+print(a[0:])
