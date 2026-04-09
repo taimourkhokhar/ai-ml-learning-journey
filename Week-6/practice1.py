266a55ea4a9d41cfe4832fa5ba22ce2d8106610c
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-10 00:55:13 +0500

    practice

--- Week-6/practice1.py
+++ Week-6/practice1.py
@@ -69,3 +69,88 @@
 
 # for i in word_count:
   
+# Write a function is_palindrome(s) that checks if a string is palindrome (ignore spaces & case)
+
+# def palindrome(str):
+#   new=list(str)
+#   new.reverse()
+#   ch="".join(new)
+#   if str==ch:
+#    print("String is palindrome")
+#   else:
+#     print("No")
+# str=input("Enter a string")
+# palindrome(str)
+
+# def maxmin(arr):
+#   max=arr[0]
+#   min=arr[0]
+#   for i in arr:
+#     if i>max:
+#       max=i
+#     if i<min:
+#       min=i
+#   print("Max and Min are",max,min)
+
+# maxmin([1,2,3,4,5])
+
+def fib(n):
+  n1,n2=0,1
+  count=0
+  if n<=0:
+    print("Enter a positive integer")
+  elif n==1:
+    print(n1)
+  else:
+    print("Fibonacci series")
+    while count<n:
+      print(n1,end=" ")
+      nth=n1+n2 #it has 0+1==1
+
+      n1=n2##here 0==1
+      n2=nth
+      count+=1
+    
+# fib(20)
+# def fibb(n):
+#   a,b=0,1
+#   for i in range(n):
+#     print(a,end=" ")
+#     a,b=b,a+b #0,1=1,0+1 in next a will 1,1=1,2 fur 2,1=2,3
+# fibb(20)
+
+
+# def fibb(n):
+#   first,second=0,1
+#   for _ in range(n):
+#     print(first,end=" ")
+
+#     next_num=first+second#1
+#     first=second # now 0 becme 1
+#     second=next_num# now second will have next value
+
+# fibb(10)
+
+# dic={
+#   "name":"Taimour",
+#   "score":{
+#     "new":22
+#   }
+# }
+
+# print(dic["score"]['new'])
+
+def merge_dicts(d1, d2):
+
+    result = d1.copy()
+    
+    for key, value in d2.items():
+        if key in result:
+            result[key] += value
+        else:
+            result[key] = value
+            
+    return result
+dict_a = {'apples': 5, 'oranges': 2}
+dict_b = {'apples': 3, 'bananas': 10}
+print(merge_dicts(dict_a, dict_b))
