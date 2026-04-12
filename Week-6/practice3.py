be00dda6bd9d3fc7cf6c79fd937330cd554fd920
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-12 23:42:17 +0500

    week 6 practice

---
+++ Week-6/practice3.py
@@ -0,0 +1,42 @@
+# from collections import Counter
+# chars = ["a","b","b","c","c","c"]
+# count=Counter(chars)
+# string=str(count)
+# new="".join(string)
+# print(new)
+
+
+# my_list = ["a","b","b","c","c","c"]
+
+# count = {}
+
+# for item in my_list:
+#     if item in count:
+#         count[item] += 1
+#     else:
+#         count[item] = 1
+
+# print(count)
+
+# from collections import Counter
+
+# my_list = ['a', 'b', 'a', 'c', 'b', 'a', 'd']
+
+# count = Counter(my_list)
+
+# for key, value in count.items():
+#     if value == 1:
+#         print(key)
+#     else:
+#         print(f"{key}: {value}")
+
+# my_list = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+# result=''
+# for ch in set(my_list):
+#   count=my_list.count(ch)
+#   if count>1:
+#     result+=ch+str(count)
+#   else:
+#     result+=ch
+
+# print(len(result))
