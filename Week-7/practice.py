616626c690dac07ceadd31ca908b0bb1ef3544b5
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-14 01:19:34 +0500

    Week 7

--- Week-7/practice.py
+++ Week-7/practice.py
@@ -5,21 +5,72 @@
 # print(next(my))
 # print(next(my))
 # print(next(my))
-class CountUpTo:
-  def __init__(self,n):
-    self.n=n
-    self.current=1
-  def __iter__(self):
-    return self
-  def __next__(self):
-    if self.current<=self.n:
-      result=self.current
-      self.current+=1
-      return result
-    else:
-      raise StopIteration
-counter=CountUpTo(5)
+# class CountUpTo:
+#   def __init__(self,n):
+#     self.n=n
+#     self.current=1
+#   def __iter__(self):
+#     return self
+#   def __next__(self):
+#     if self.current<=self.n:
+#       result=self.current
+#       self.current+=1
+#       return result
+#     else:
+#       raise StopIteration
+# counter=CountUpTo(5)
 
-for num in counter:
-  print(num)
+# for num in counter:
+#   print(num)
 
+# str1="taimour"
+# str2=list(str1)
+# str2.reverse()
+# new="".join(str2)
+# my=iter(new)
+# print(next(my))
+# print(next(my))
+# print(next(my))
+# print(next(my))
+
+# print(str[::-1])
+# for i in str1:
+#   print(i)
+
+
+# 3. Even Numbers Iterator
+
+# Create a class that:
+
+# Takes a limit n
+# Iterates only even numbers up to n
+
+# 👉 Example:
+
+# n = 10 → 2,4,6,8,10
+
+# class Even:
+#   def __init__(self,n):
+#     self.n=n
+#     self.current=1
+#   def __iter__(self):
+#     return self
+#   def __next__(self):
+#     if self.current<=self.n:
+#       res=self.current
+#       self.current+=1
+#       val=res%2==0
+#       return val
+#     else:
+#       raise StopIteration
+# n=10
+# nw=Even(n)
+# for i in nw:
+#   print(i)
+
+f=open("D:/ai engineer roadmap/funny.txt",'r')
+new=f.read()
+f.close()
+nw=iter(new.splitlines())
+print(next(nw))
+print(next(nw))
