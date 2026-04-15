0296655a21c31cc8cd1b9799cd6f38cbd108c80b
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-15 23:43:32 +0500

    week 7 work update

--- Week-7/practice1.py
+++ Week-7/practice1.py
@@ -18,14 +18,69 @@
 
 # print(vl.size)
 
-nums=[0,1,0,3,12]
-index=0
+# nums=[0,1,0,3,12]
+# index=0
 
-for i in range(len(nums)):
-  if nums[i]!=0:# 
-    temp=nums[i]
-    nums[i]=nums[index]
-    nums[index]=temp
-    print(nums[i])
-    index+=1
-print(nums)
+# for i in range(len(nums)):
+#   if nums[i]!=0:# 
+#     temp=nums[i]
+#     nums[i]=nums[index]
+#     nums[index]=temp
+#     print(nums[i])
+#     index+=1
+# print(nums)
+
+# num=5
+# class counter:
+#   def __init__(self,num) -> None:
+#     self.num=num
+#     self.count=1
+#   def __iter__(self):
+#     return self
+#   def __next__(self):
+#     if self.count<=self.num:
+#      res=self.count
+#      self.count+=1
+#      return res
+#     else:
+#        raise StopIteration
+
+
+
+
+
+# my_counter=counter(num)
+# for i in my_counter:
+#   print(i)
+
+
+
+
+# ls=[10,20,30,40]
+
+# my=iter(ls)
+
+# print(next(my))
+
+#Write a generator that yields numbers from 1 to 10.
+
+# def count(n):
+#   count=1
+#   while count<=n:
+#     yield count
+#     count+=1
+# gen=count(5)
+# print(next(gen))
+
+
+# def square(n):
+#   start=1
+#   while start<=n:
+#     yield start*start
+#     start+=1
+# gen=square(10)
+
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
