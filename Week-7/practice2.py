6b89b29d4776718d6d7452023fa261dc05b56671
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-19 12:28:00 +0500

    Week 7 work

--- Week-7/practice2.py
+++ Week-7/practice2.py
@@ -1,18 +1,31 @@
-nums = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
+# nums = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
 
-k = 2
+# k = 2
+# left=0
+# right=len(nums)-1
+# count=0
+# while left<right:
+#   current_sum=nums[left]+nums[right]
+#   if current_sum==k:
+#     count+=1
+#     left+=1
+#     right-=1
+#   elif current_sum<k:
+#       left+=1
+#   else:
+#      right-=1
+
+# print(count)
+
+
+nums=[0,0,0,1,1,1]
 left=0
 right=len(nums)-1
-count=0
-while left<right:
-  current_sum=nums[left]+nums[right]
-  if current_sum==k:
-    count+=1
-    left+=1
-    right-=1
-  elif current_sum<k:
-      left+=1
-  else:
-     right-=1
-
-print(count)
+for i in nums:
+  if nums[i]==0:
+     temp=nums[right]
+     nums[right]=nums[left]
+     nums[left]=temp
+  left+=1
+  right-=1
+print(nums)
