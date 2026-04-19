1adba1a6766e10d83a6bf1258369b0161e6388e8
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-19 13:08:11 +0500

    Week 7 practice

--- Week-7/practice2.py
+++ Week-7/practice2.py
@@ -18,14 +18,38 @@
 # print(count)
 
 
-nums=[0,0,0,1,1,1]
-left=0
-right=len(nums)-1
-for i in nums:
-  if nums[i]==0:
-     temp=nums[right]
-     nums[right]=nums[left]
-     nums[left]=temp
-  left+=1
-  right-=1
+# nums=[0,0,0,1,1,1]
+# left=0
+# right=len(nums)-1
+# for i in nums:
+#   if nums[i]==0:
+#      temp=nums[right]
+#      nums[right]=nums[left]
+#      nums[left]=temp
+#   left+=1
+#   right-=1
+# print(nums)
+
+
+# 1. Move All Zeros to End
+
+# Given:
+
+# nums = [0,1,0,3,12]
+
+# Move all 0s to the end while keeping order of non-zero elements
+nums = [0, 1, 0, 1, 3, 12]
+left = 0
+right = 0 
+
+while right < len(nums):# here loop runs right<len 
+    if nums[right] != 0:# if nums[right]!=0 like at index 1 is 1 value so we move in
+        temp = nums[left]# simple swaping
+        nums[left] = nums[right]
+        nums[right] = temp
+        
+        left += 1# we move left+=1 so left will be 0 now at 1 index due to swaping
+    
+    right += 1# here right will move forward
+
 print(nums)
