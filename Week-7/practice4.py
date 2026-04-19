9e621059ad2df9788a0f58de0c7e45775cd32919
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-19 22:01:11 +0500

    Week 7 work

--- Week-7/practice4.py
+++ Week-7/practice4.py
@@ -69,3 +69,28 @@
 #     i+=1
 
 # print(nums)
+
+
+
+
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+
+
+# nums=[1]
+# counts = {}
+# threshold = len(nums) / 2
+# print(threshold)
+# for i in nums:
+#   counts[i] = counts.get(i, 0) + 1
+            
+#   if counts[i] > threshold:
+#          print( i)
