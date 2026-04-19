0f0263d5de18f402520f02794529ce905dbd49fd
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-19 15:46:27 +0500

    week 7 update

--- Week-7/practice2.py
+++ Week-7/practice2.py
@@ -38,18 +38,63 @@
 # nums = [0,1,0,3,12]
 
 # Move all 0s to the end while keeping order of non-zero elements
-nums = [0, 1, 0, 1, 3, 12]
-left = 0
-right = 0 
+# nums = [0, 1, 0, 1, 3, 12]
+# left = 0
+# right = 0 
 
-while right < len(nums):# here loop runs right<len 
-    if nums[right] != 0:# if nums[right]!=0 like at index 1 is 1 value so we move in
-        temp = nums[left]# simple swaping
-        nums[left] = nums[right]
-        nums[right] = temp
+# while right < len(nums):# here loop runs right<len 
+#     if nums[right] != 0:# if nums[right]!=0 like at index 1 is 1 value so we move in
+#         temp = nums[left]# simple swaping
+#         nums[left] = nums[right]
+#         nums[right] = temp
         
-        left += 1# we move left+=1 so left will be 0 now at 1 index due to swaping
+#         left += 1# we move left+=1 so left will be 0 now at 1 index due to swaping
     
-    right += 1# here right will move forward
+#     right += 1# here right will move forward
 
-print(nums)
+# print(nums)
+
+
+
+# 2. Reverse a List Using Two Pointers
+
+# Given:
+
+# nums = [1,2,3,4,5]
+
+# Reverse it in-place.
+
+# nums=[1,2,3,4,5]
+
+# left=0
+# right=len(nums)-1
+
+# while left<right:
+#     temp=nums[left]
+#     nums[left]=nums[right]
+#     nums[right]=temp
+#     left+=1
+#     right-=1
+
+# print(nums)
+
+
+# 3. Remove Duplicates from Sorted Array
+
+# Given:
+
+# nums = [1,1,2,2,3,4,4]
+
+# Remove duplicates in-place and return unique part.
+# nums = [1,1,2,2,3,4,4]
+
+# left=0
+# right=0
+
+# while right<len(nums):
+#   if nums[left]==nums[right+1]:
+#     del nums[left]
+#   left+=1
+#   right+=1
+
+# print(nums)
---
+++ Week-7/practice4.py
@@ -0,0 +1,71 @@
+# 4. Two Sum (Sorted Array)
+# nums = [1,2,3,4,6]
+# target = 6
+
+# Find two numbers whose sum = target.
+
+# nums = [1,2,3,4,6]
+# target = 5
+
+# left=0
+# right=1
+
+
+# while right<len(nums):
+#   if nums[left]+nums[right]==target:
+#     print(nums[left],"and",nums[right])
+#   left+=1
+#   right+=1
+
+
+# 1. Check Palindrome
+# word = "madam"
+
+# Return True if palindrome.
+
+# word="madam"
+# def check_pal(word):
+#  if word==word[::-1]:
+#   return True
+#  else:
+#   return False
+ 
+# print(check_pal(word))
+
+# height = [1,8,6,2,5,4,8,3,7]
+# left=0
+# right=len(height)-1
+# maxe=0
+
+# while left<right:
+#   width=right-left
+#   max_area=width*min(height[left],height[right])
+#   if max_area>maxe:
+#    maxe=max_area
+#   if height[left]<height[right]:
+#     left+=1
+#   else:
+#     right-=1
+# print(maxe)
+
+
+# nums = [2,0,2,1,1,0]
+# left=0
+# i=0
+# right=len(nums)-1
+# while i<=right:
+#   if nums[i]==0:
+#     temp=nums[left]
+#     nums[left]=nums[i]
+#     nums[i]=temp
+#     left+=1
+#     i+=1
+#   elif nums[i]==2:
+#     temp=nums[right]
+#     nums[right]=nums[i]
+#     nums[i]=temp
+#     right-=1
+#   else:
+#     i+=1
+
+# print(nums)
