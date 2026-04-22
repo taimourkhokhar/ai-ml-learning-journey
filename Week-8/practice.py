cffa6bca7ff4f60489baf734b1fa5694096ebd16
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-22 23:32:31 +0500

    Week 8 work

--- Week-8/practice.py
+++ Week-8/practice.py
@@ -17,3 +17,25 @@
 # s = "Hello World "
 # new=s.split()
 # print(len(new[-1]))
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Map each Roman numeral to its integer value
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+                
+        return total
