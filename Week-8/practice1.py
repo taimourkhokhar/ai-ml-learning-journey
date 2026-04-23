24c74c8b37421cc2c20b35b71262530df25e1d29
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-23 18:53:52 +0500

    Week 8 work

--- Week-8/practice1.py
+++ Week-8/practice1.py
@@ -1,18 +1,31 @@
-import math
+# import math
 
-num = 6
-is_prime = True
+# num = 6
+# is_prime = True
 
-if num <= 1:
-    is_prime = False
-else:
-    # Check only up to the square root
-    for i in range(2, int(math.sqrt(num)) + 1):
-        if num % i == 0:
-            is_prime = False
-            break
+# if num <= 1:
+#     is_prime = False
+# else:
+#     # Check only up to the square root
+#     for i in range(2, int(math.sqrt(num)) + 1):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+
+# if is_prime:
+#     print(f"{num} is prime")
+# else:
+#     print(f"{num} is not prime")
+
+nums=[1,2,3,4,5,6,7,8,9]
+is_prime=True
+for i in nums:
+  if i%2==0:
+    is_prime=False
+  if i%i==0:
+    is_prime=False
 
 if is_prime:
-    print(f"{num} is prime")
+  print("prime numbers are",nums)
 else:
-    print(f"{num} is not prime")
+  print("Not a prime numbers")
