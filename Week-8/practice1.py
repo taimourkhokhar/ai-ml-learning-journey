9201b1b1e863f04318cee4d7c7be942cfd3786a1
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-24 01:37:24 +0500

    week 8 work

--- Week-8/practice1.py
+++ Week-8/practice1.py
@@ -7,7 +7,7 @@
 #     is_prime = False
 # else:
 #     # Check only up to the square root
-#     for i in range(2, int(math.sqrt(num)) + 1):
+#     for i in range(2,num):
 #         if num % i == 0:
 #             is_prime = False
 #             break
@@ -18,14 +18,22 @@
 #     print(f"{num} is not prime")
 
 nums=[1,2,3,4,5,6,7,8,9]
-is_prime=True
-for i in nums:
-  if i%2==0:
-    is_prime=False
-  if i%i==0:
-    is_prime=False
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+new = []
 
-if is_prime:
-  print("prime numbers are",nums)
-else:
-  print("Not a prime numbers")
+for i in nums:
+    if i <= 1:
+        continue  # Skip 1 because it's not prime
+    
+    is_prime = True  # Assume it's prime until proven otherwise
+    
+    # Test divisors from 2 up to the number itself
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break  # Found a divisor, stop checking this number
+            
+    if is_prime:
+        new.append(i)
+
+print(new)  # Output: [2, 3, 5, 7]
