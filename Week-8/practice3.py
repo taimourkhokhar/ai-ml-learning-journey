9435a2df7dbd15b1f4f8e105a4ddc1202f53d85e
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-26 22:39:54 +0500

    Week 8 practice 3

--- Week-8/practice3.py
+++ Week-8/practice3.py
@@ -1,9 +1,30 @@
-text="python is great and python is easy"
-dic={}
-for i in text.split():
-  if i not in dic:
-    dic[i]=1
-  else:
-    dic[i]+=1
+# text="python is great and python is easy"
+# dic={}
+# for i in text.split():
+#   if i not in dic:
+#     dic[i]=1
+#   else:
+#     dic[i]+=1
 
-print(dic)
+# print(dic)
+
+
+#sliding window
+
+nums = [2,1,5,1,3,2]
+k = 3
+
+window_sum = sum(nums[:k])   # first 3 elements
+max_sum = window_sum
+
+print("First window:", nums[:k], "=", window_sum)
+
+for i in range(k, len(nums)):
+    window_sum = window_sum - nums[i-k] + nums[i]
+    
+    print("New window sum:", window_sum)
+
+    if window_sum > max_sum:
+        max_sum = window_sum
+
+print("Maximum sum:", max_sum/k)
