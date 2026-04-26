e0c7cc2c5a7e8e88840a66004b64bd8628422805
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-26 23:56:46 +0500

    numpy practice

---
+++ Week-8/numpy1.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# array=np.array([1,2,3,4,5])
+# print(type(array))
+# print(np.__version__)
+
+# #we can also create tuple for numpy array
+# arr = np.array((1, 2, 3, 4, 5))
+
+# print(arr)
+
+
+#dimension in numpy array
+# arr=np.array(42)
+# print(arr.ndim)
+
+
+
+# a=np.array(42)
+# b=np.array([1,2,3,4])
+# c=np.array([[1,2,3,4],[5,6,7,8]])
+# d=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+
+
+# print(a.ndim)
+# print(b.ndim)
+# print(c.ndim)
+# print(d.ndim)
+
+
+# arr = np.array([1, 2, 3, 4], ndmin=5)
+
+# print(arr)
+# print('number of dimensions :', arr.ndim)
