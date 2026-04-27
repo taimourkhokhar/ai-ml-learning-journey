258d0af5124c38e1e6ddccb83f9e37ce880b344b
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-27 21:29:27 +0500

    week 8 practice 3

--- Week-8/practice3.py
+++ Week-8/practice3.py
@@ -11,20 +11,22 @@
 
 #sliding window
 
-nums = [2,1,5,1,3,2]
-k = 3
+# nums = [2,1,5,1,3,2]
+# k = 3
 
-window_sum = sum(nums[:k])   # first 3 elements
-max_sum = window_sum
+# window_sum = sum(nums[:k])   # first 3 elements
+# max_sum = window_sum
 
-print("First window:", nums[:k], "=", window_sum)
+# print("First window:", nums[:k], "=", window_sum)
 
-for i in range(k, len(nums)):
-    window_sum = window_sum - nums[i-k] + nums[i]
+# for i in range(k, len(nums)):
+#     window_sum = window_sum - nums[i-k] + nums[i]
     
-    print("New window sum:", window_sum)
+#     print("New window sum:", window_sum)
 
-    if window_sum > max_sum:
-        max_sum = window_sum
+#     if window_sum > max_sum:
+#         max_sum = window_sum
 
-print("Maximum sum:", max_sum/k)
+# print("Maximum sum:", max_sum/k)
+
+
