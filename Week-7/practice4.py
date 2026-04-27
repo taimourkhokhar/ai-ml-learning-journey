f8a2aacc9ea2411b6b3def25fe41043415574a8b
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-28 02:29:55 +0500

    week 7 practice 4

--- Week-7/practice4.py
+++ Week-7/practice4.py
@@ -94,3 +94,84 @@
             
 #   if counts[i] > threshold:
 #          print( i)
+
+
+
+# st="taaatmi"
+
+# dic={}
+
+# for i in st:
+#   if i not in dic:
+#     dic[i]=1
+#   else:
+#     dic[i]+=1
+
+# print(dic)
+
+
+# st=["python","python","java","javascript"]
+
+# dic={}
+# for i in st:
+#   if i not in dic:
+#     dic[i]=1
+#   else:
+#     dic[i]+=1
+
+# print(dic)
+
+#do like if st has python move it to end 
+
+# st=["python","python","java","javascript"]
+# left=0
+# right=len(st)-1
+
+# while left<right:
+#   if st[left]!="python":
+#     left+=1
+#   elif st[right]=="python":
+#     right-=1
+#   else:
+#     st[left],st[right]=st[right],st[left]
+#     left+=1
+#     right-=1
+# print(st)
+
+ 
+# nums=[0,0,0,1,1,1]
+# left=0
+# right=len(nums)-1
+# for i in nums:
+#   if nums[i]==0:
+#      temp=nums[right]
+#      nums[right]=nums[left]
+#      nums[left]=temp
+#   left+=1
+#   right-=1
+# print(nums)
+
+
+nums = [0,2,1,0,1,2,1]
+
+left = 0
+now = 0
+right = len(nums) - 1
+
+while now <= right:
+
+    if nums[now] == 0:
+        nums[left], nums[now] = nums[now], nums[left]
+        left += 1
+        now += 1
+
+    elif nums[now] == 1:
+        now += 1
+
+    else:   # nums[now] == 2
+        nums[now], nums[right] = nums[right], nums[now]
+        right -= 1
+
+    print(nums)
+
+print("Final:", nums)
