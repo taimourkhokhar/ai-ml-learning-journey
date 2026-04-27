cbbc2c6e3b77d23031f42671e26f9525d2b9cf6e
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-27 22:57:34 +0500

    Week 8 numpy practice

--- Week-8/numpy1.py
+++ Week-8/numpy1.py
@@ -32,3 +32,51 @@ import numpy as np
 
 # print(arr)
 # print('number of dimensions :', arr.ndim)
+
+#slicing in numpy array
+
+# arr=np.array([1,2,3,4,5,6])
+# print(arr[0]+arr[2])
+
+# arr=np.array([[1,2,3,4],[5,6,7,8]])
+# print(arr[1,1])
+# #now accessing second row 4 column
+
+# print(arr[1,3])
+
+
+# arr=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+# print(arr[0,1,2])
+# print(arr[0,1,1])
+# print(arr[0,1,-1])
+
+#slicing in numpy array
+
+# arr=np.array([1,2,3,4,5])
+# print(arr[-3:-1])
+
+
+#numpy data types strings,integer,boolean ,float,complex
+
+# arr=np.array([1,2,3,4,5],dtype='i4')
+# print(arr.dtype)
+# arr1=np.array(["string"])
+# print(arr1.dtype)
+
+
+
+# arr=np.array(["apple","banana","cherry"],dtype='i')
+# print(arr.dtype)
+
+
+# arr=np.array([1.1,2.2,3.3,4.4,5.5])
+
+# newArr=arr.astype('i')
+
+# print(arr.dtype)
+# print(newArr.dtype)
+
+# arr=np.array([-1,0,3])
+# newArr=arr.astype(bool)
+
+# print(newArr)
