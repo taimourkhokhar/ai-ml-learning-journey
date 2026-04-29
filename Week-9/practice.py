b7d77b1e987f828746b2f6c4340bcacc7780ffad
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-29 22:09:40 +0500

    Week 9 practice

--- Week-9/practice.py
+++ Week-9/practice.py
@@ -1,37 +1,57 @@
-#reverse array without reverse
-# nums=[1,2,3,4,5]
+# #reverse array without reverse
+# # nums=[1,2,3,4,5]
+# # left=0
+# # right=len(nums)-1
+
+# # while left<right:
+# #   nums[left],nums[right]=nums[right],nums[left]
+# #   left+=1
+# #   right-=1
+
+# # print(nums)
+
+# #how many nums are repeated in array
+# # nums=[1,2,3,2,4,5]
+# # repeat=[]
+
+# # for i in nums:
+# #   if i not in repeat:
+# #     repeat.append(i)
+# #   else:
+# #     print(i)
+
+
+# #tow pointer moves zero to end
+
+# nums=[0,0,0,1,1,1]
+
 # left=0
 # right=len(nums)-1
 
 # while left<right:
-#   nums[left],nums[right]=nums[right],nums[left]
-#   left+=1
-#   right-=1
+#   if nums[left]==0:
+#     nums[left],nums[right]=nums[right],nums[left]
+#     left+=1
+#     right-=1
 
 # print(nums)
-
-#how many nums are repeated in array
-# nums=[1,2,3,2,4,5]
-# repeat=[]
-
-# for i in nums:
-#   if i not in repeat:
-#     repeat.append(i)
-#   else:
-#     print(i)
-
-
-#tow pointer moves zero to end
-
-nums=[0,0,0,1,1,1]
-
+#leet code 167
+numbers = [2,7,11,15]
+target = 9
 left=0
-right=len(nums)-1
-
+right=len(numbers)-1
+sum_target=0
+arr=[]
 while left<right:
-  if nums[left]==0:
-    nums[left],nums[right]=nums[right],nums[left]
-    left+=1
+  sum_target=numbers[left]+numbers[right]
+  if sum_target==target:
+    arr.append(left+1)
+    arr.append(right+1)
+    break
+  elif sum_target>target:
     right-=1
+  else:
+    left+=1
 
-print(nums)
+
+print(arr)
