b8d006c2c2e9a42150738cd5b5c9ebe7abcb5b66
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-30 22:56:16 +0500

    Week 9 work

--- Week-9/practice.py
+++ Week-9/practice.py
@@ -36,22 +36,32 @@
 
 # print(nums)
 #leet code 167
-numbers = [2,7,11,15]
-target = 9
+# numbers = [2,7,11,15]
+# target = 9
+# left=0
+# right=len(numbers)-1
+# sum_target=0
+# arr=[]
+# while left<right:
+#   sum_target=numbers[left]+numbers[right]
+#   if sum_target==target:
+#     arr.append(left+1)
+#     arr.append(right+1)
+#     break
+#   elif sum_target>target:
+#     right-=1
+#   else:
+#     left+=1
+
+
+# print(arr)
+
+nums = [0,0,1,1,1,2,2,3,3,4]
 left=0
-right=len(numbers)-1
-sum_target=0
-arr=[]
-while left<right:
-  sum_target=numbers[left]+numbers[right]
-  if sum_target==target:
-    arr.append(left+1)
-    arr.append(right+1)
-    break
-  elif sum_target>target:
-    right-=1
-  else:
-    left+=1
-
-
-print(arr)
+right=0
+while right<len(nums):
+   if nums[right]!=nums[left]:
+      left+=1
+      nums[left]=nums[right]
+   right+=1
+print(nums[:left+1])
