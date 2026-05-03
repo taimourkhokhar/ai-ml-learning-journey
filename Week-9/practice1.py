a91c30a740372cd3ce062c7a1900cabcc49bb605
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-03 23:32:43 +0500

    Week 9 practice 1 update

--- Week-9/practice1.py
+++ Week-9/practice1.py
@@ -63,4 +63,4 @@ arr = np.array([[1, 2, 3],[4,5,6]])
 # arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])### for iterating if dimension more than 3
 
 # for x in np.nditer(arr):
-#   print(x)
+#   print(x) 
