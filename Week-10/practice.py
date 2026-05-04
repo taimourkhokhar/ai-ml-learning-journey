0ce1f6b03795f2b6d6bb79c51d829a697a4d8330
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-04 22:54:42 +0500

    Week 10 practice update

--- Week-10/practice.py
+++ Week-10/practice.py
@@ -1,17 +1,22 @@
-nums = [1,2,3,4]
-k = 5
-left=0
-count=0
-right=len(nums)-1
-while left<right:
-  sums=nums[left]+nums[right]
-  if sums==k:
-    count=count+1
-    left+=1
-    right-=1
-  elif sums<k:
-    left+=1
-  else:
-    right-=1
+nums = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
+k = 2
 
-print(count)
+nums.sort() 
+
+left = 0
+right = len(nums) - 1
+count = 0
+
+while left < right:
+    sums = nums[left] + nums[right]
+    
+    if sums == k:
+        count += 1
+        left += 1
+        right -= 1
+    elif sums < k:
+        left += 1
+    else:
+        right -= 1
+
+print(f"Pairs found: {count}")
