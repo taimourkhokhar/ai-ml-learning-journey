4b3d5ec00c8fadba4323f6c02e2337837938316f
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-13 02:22:29 +0500

    week 11 practice

--- Week-11/practice.py
+++ Week-11/practice.py
@@ -1,22 +1,22 @@
-s = "abcabcbb"
+# s = "abcabcbb"
 
-char_set = set()
-left = 0
-max_length = 0
+# char_set = set()
+# left = 0
+# max_length = 0
 
-for right in range(len(s)):
-    # 1. If we see a duplicate, shrink from the left until it's gone
-    while s[right] in char_set:
-        char_set.remove(s[left])
-        left += 1
+# for right in range(len(s)):
+#     # 1. If we see a duplicate, shrink from the left until it's gone
+#     while s[right] in char_set:
+#         char_set.remove(s[left])
+#         left += 1
 
-    # 2. Add the current character (now guaranteed to be unique in the window)
-    char_set.add(s[right])
+#     # 2. Add the current character (now guaranteed to be unique in the window)
+#     char_set.add(s[right])
     
-    # 3. Update the max length seen so far
-    max_length = max(max_length, right - left + 1)
+#     # 3. Update the max length seen so far
+#     max_length = max(max_length, right - left + 1)
 
-print(max_length) # Output: 3
+# print(max_length) # Output: 3
 
 
 # import numpy as np
@@ -36,3 +36,25 @@ print(max_length) # Output: 3
 # for x in data:
 #   if x<lowerlimit or x>upperlimit:
 #     print("ourtlier is",x)
+
+
+
+
+
+# from scipy import stats
+# import numpy as np
+
+# data = [10,12,11,13,12,14,500]
+
+# z_scores = np.abs(stats.zscore(data))
+
+# print(z_scores)
+
+
+# filtered = []
+
+# for i in range(len(data)):
+#     if z_scores[i] < 3:
+#         filtered.append(data[i])
+
+# print(filtered)
