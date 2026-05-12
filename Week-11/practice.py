621e026daee86c7f25d2f8c7cbdcd29c6561ddf9
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-12 22:44:13 +0500

    Week 11 work update

--- Week-11/practice.py
+++ Week-11/practice.py
@@ -17,3 +17,22 @@ for right in range(len(s)):
     max_length = max(max_length, right - left + 1)
 
 print(max_length) # Output: 3
+
+
+# import numpy as np
+
+# data = [5,7,8,10,12,15,18,20,100]
+
+# q1=np.percentile(data,25)
+# print(q1)
+# q3=np.percentile(data,75)
+# print(q3)
+
+# iqr=q3-q1
+
+# lowerlimit=q1-1.5*iqr
+# upperlimit=q3+1.5*iqr
+
+# for x in data:
+#   if x<lowerlimit or x>upperlimit:
+#     print("ourtlier is",x)
