1315c473f658467546e15be656f6410e1cb27b01
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-13 23:18:52 +0500

    week 11 practice

--- Week-11/practice.py
+++ Week-11/practice.py
@@ -58,3 +58,43 @@
 #         filtered.append(data[i])
 
 # print(filtered)
+
+
+# nums = [0,1,1,1,0,1,1,0,1]
+
+# left = 0
+# zero_count = 0
+# max_len = 0
+
+# for right in range(len(nums)):## right is at index 0 also 
+
+#     if nums[right] == 0:# check if nums of right i mean at 0 index it has zero value so it  is tur
+#         zero_count += 1## zero count will be one
+
+#     while zero_count > 1: #check zero count greater than 1 or not because we have to maintain at least one zero
+
+#         if nums[left] == 0:# check if nums left=0
+#             zero_count -= 1
+
+#         left += 1
+
+#     max_len = max(max_len, right - left)
+
+# print(max_len)
+
+
+
+arr = [2,1,5,1,3,2]
+k = 3
+window=sum(arr[:k])
+
+left=0
+maxsum=window
+for right in range(k,len(arr)):
+  window=window-arr[left]+arr[right]
+  left+=1
+  if window>maxsum:
+    maxsum=window
+    
+    
+print(maxsum)
