b47d023dacca4fab7b560106cccd8a80902f1d57
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-20 21:39:34 +0500

    update

--- Week-12/practice.py
+++ Week-12/practice.py
@@ -1,12 +1,26 @@
-arr = [3, 1, 6, 2, 8, 4]
+# arr = [3, 1, 6, 2, 8, 4]
 
-prefix = [0] * len(arr)
-print(prefix)
+# prefix=[0] * len(arr)
 
-prefix[0] = arr[0]
-print(prefix)
 
-for i in range(1, len(arr)):
-    prefix[i] = prefix[i - 1] + arr[i]
+# prefix[0]=arr[0]
 
-    print(prefix)
+
+# for i in range(1,len(arr)):
+#   prefix[i]=arr[i-1]+arr[i]
+  
+# print(prefix)
+
+
+
+
+gain = [-5,1,5,0,-7]
+
+current=0
+altitude=0
+
+for g in gain:
+    current+=g
+    
+    altitude=max(altitude,current)
+print(altitude)
