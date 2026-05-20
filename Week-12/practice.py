d32ce5b29c626705ceed316420f107ec0f90fa8d
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-20 23:02:22 +0500

    practice leetcode question

--- Week-12/practice.py
+++ Week-12/practice.py
@@ -14,13 +14,47 @@
 
 
 
-gain = [-5,1,5,0,-7]
+# gain = [-5,1,5,0,-7]
 
-current=0
-altitude=0
+# current=0
+# altitude=0
 
-for g in gain:
-    current+=g
+# for g in gain:
+#     current+=g
     
-    altitude=max(altitude,current)
-print(altitude)
+#     altitude=max(altitude,current)
+# print(altitude)
+
+
+
+
+#leeet code pivot index
+
+
+
+
+# prefix=[0]*len(nums)
+
+# prefix[0]=nums[0]
+
+# for i in range(1,len(nums)):
+#     prefix[i]=nums[i-1]+nums[i]
+    
+# print(prefix)
+nums = [1,7,3,6,5,6]
+
+totalsum=0
+
+leftsum=0
+rightsum=0
+leftptr=0
+
+for i in nums:
+    totalsum+=i
+
+for left in range(len(nums)):
+    leftsum=sum(nums[:left])
+    rightsum=sum(nums[left + 1:])
+    if leftsum==rightsum:
+        print(left)
+
