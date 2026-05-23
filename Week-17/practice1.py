137d7047296d007ad0e76b04d75334afd4987314
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-23 13:35:23 +0500

    practice1

---
+++ Week-17/practice1.py
@@ -0,0 +1,42 @@
+name="Taimour"
+age=23
+city="Lahore"
+print(f"My name is {name}, age is {age} and city is {city}")
+
+a = 20
+b = 5
+
+print("sum is ",a+b)
+print("subtraction is ",a-b)
+print("multiplication is ",a*b)
+print("division is ",a/b)
+
+text = "Python"
+
+print("p")
+print("n")
+print(text,text,text)
+
+length = 10
+width = 5
+
+
+print("area of rectangle is ",length*width)
+
+
+sentence = "I love machine learning"
+count=0
+
+for snt in sentence:
+  count+=1
+
+
+
+lwr=sentence.lower()
+upr=sentence.upper()
+print(count)
+print(lwr)
+print(upr)
+
+
+
