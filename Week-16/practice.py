e3cef9b62aae05e7905c99e68b46ed006c205fc1
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-06-16 22:29:12 +0500

    update practice

--- Week-16/practice.py
+++ Week-16/practice.py
@@ -1,28 +1,105 @@
-import numpy as np
+# import numpy as np
 
-arr = np.array([41, 42, 43, 44])
+# arr = np.array([41, 42, 43, 44])
 
-# Create an empty list
-filter_arr = []
+# # Create an empty list
+# filter_arr = []
 
-# go through each element in arr
-for element in arr:
-  # if the element is higher than 42, set the value to True, otherwise False:
-  if element > 42:
-    filter_arr.append(True)
-  else:
-    filter_arr.append(False)
+# # go through each element in arr
+# for element in arr:
+#   # if the element is higher than 42, set the value to True, otherwise False:
+#   if element > 42:
+#     filter_arr.append(True)
+#   else:
+#     filter_arr.append(False)
 
-newarr = arr[filter_arr]
+# newarr = arr[filter_arr]
 
-print(filter_arr)
-print(newarr)
-
-
+# print(filter_arr)
+# print(newarr)
 
 
-arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 
-newarr = arr.reshape(2, 2, -1)
 
-print(newarr)
+# arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+# newarr = arr.reshape(2, 2, -1)
+
+# print(newarr)
+# class Person:
+#   def __init__(self, fname, lname):
+#     self.firstname = fname
+#     self.lastname = lname
+
+#   def printname(self):
+#     print(self.firstname, self.lastname)
+
+# #Use the Person class to create an object, and then execute the printname method:
+
+# x = Person("John", "Doe")
+# x.printname()
+
+
+
+
+# class Car:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Drive!")
+
+# class Boat:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Sail!")
+
+# class Plane:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
+
+#   def move(self):
+#     print("Fly!")
+
+# car1 = Car("Ford", "Mustang")       #Create a Car object
+# boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+# plane1 = Plane("Boeing", "747")     #Create a Plane object
+
+# for x in (car1, boat1, plane1):
+#   x.move()
+  
+  
+  
+  
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang")       #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747")     #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
