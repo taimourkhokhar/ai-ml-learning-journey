7f53594940586018a1a2fccf6b9b68cc4c195f73
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-06-25 20:25:03 +0500

    update practice1

--- Week-17/practice1.py
+++ Week-17/practice1.py
@@ -1,73 +1,134 @@
-name="Taimour"
-age=23
-city="Lahore"
-print(f"My name is {name}, age is {age} and city is {city}")
+# name="Taimour"
+# age=23
+# city="Lahore"
+# print(f"My name is {name}, age is {age} and city is {city}")
 
-a = 20
-b = 5
+# a = 20
+# b = 5
 
-print("sum is ",a+b)
-print("subtraction is ",a-b)
-print("multiplication is ",a*b)
-print("division is ",a/b)
+# print("sum is ",a+b)
+# print("subtraction is ",a-b)
+# print("multiplication is ",a*b)
+# print("division is ",a/b)
 
-text = "Python"
+# text = "Python"
 
-print("p")
-print("n")
-print(text,text,text)
+# print("p")
+# print("n")
+# print(text,text,text)
 
-length = 10
-width = 5
-
-
-print("area of rectangle is ",length*width)
+# length = 10
+# width = 5
 
 
-sentence = "I love machine learning"
-count=0
-
-for snt in sentence:
-  count+=1
+# print("area of rectangle is ",length*width)
 
 
+# sentence = "I love machine learning"
+# count=0
 
-lwr=sentence.lower()
-upr=sentence.upper()
-print(count)
-print(lwr)
-print(upr)
+# for snt in sentence:
+#   count+=1
 
 
 
-#list
-fruit=["mango","orange","apple","blueberry"]
-print(fruit[0])
-print(fruit[-1])
+# lwr=sentence.lower()
+# upr=sentence.upper()
+# print(count)
+# print(lwr)
+# print(upr)
 
-numbers = [10,20,30,40,50]
-numbers.append(60)
-print(numbers)
 
-numbers = [5,10,15,20,25]
 
-sum=0
-for num in numbers:
-  sum+=num
+# #list
+# fruit=["mango","orange","apple","blueberry"]
+# print(fruit[0])
+# print(fruit[-1])
 
-print("sum of numbers is ",sum)
+# numbers = [10,20,30,40,50]
+# numbers.append(60)
+# print(numbers)
 
-names = ["Ali","Sara","Ahmed","Fatima"]
+# numbers = [5,10,15,20,25]
 
-for name in names:
-  print(name)
+# sum=0
+# for num in numbers:
+#   sum+=num
 
-numbers = [1,2,3,4,5]
+# print("sum of numbers is ",sum)
 
-sume=[]
+# names = ["Ali","Sara","Ahmed","Fatima"]
 
-for num in numbers:
-  sumer=num*num
-  sume.append(sumer)
+# for name in names:
+#   print(name)
 
-print(sume)
+# numbers = [1,2,3,4,5]
+
+# sume=[]
+
+# for num in numbers:
+#   sumer=num*num
+#   sume.append(sumer)
+
+# print(sume)
+
+# student = {
+#     "name":"Ali",
+#     "age":22,
+#     "city":"Lahore"
+# }
+
+
+# print(student["name"])
+
+# student['course']="python"
+
+# print(student)
+# for key,value in student.items():
+#   print(key,value)
+  
+  
+#   marks = {
+#     "Math":90,
+#     "English":85,
+#     "Physics":95
+# }
+  
+# total_marks=0
+
+# for key,value in marks.items():
+#   total_marks+=value
+  
+# print("total marks is ",total_marks)
+
+
+
+# set1={1,2,3,4,5}
+# print(set1)
+# set1.add(6)
+# print(set1)
+# set1.remove(3)
+# print(set1)
+
+
+# set1 = {1,2,3}
+# set2 = {3,4,5}
+# uni=set1.union(set2)
+# print(uni)
+# inter=set1.intersection(set2)
+# print(inter)
+
+# tp=("Ali",22,"Lahore")
+# print(tp)
+# print(tp[1])
+
+
+# numbers = (1,2,3,1,1,5)
+# count=0
+# for nm in numbers:
+#   if nm==1:
+#     count+=1
+    
+# print(count)
+# print(len(numbers))
+# print(type(list(numbers)))
