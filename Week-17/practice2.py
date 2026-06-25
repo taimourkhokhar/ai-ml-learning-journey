62b96878a2fc07f64fd7117d7034ec86f33622d0
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-06-25 20:55:03 +0500

    practice2

---
+++ Week-17/practice2.py
@@ -0,0 +1,46 @@
+# num=5
+# if num>=0:
+#   print("positive")
+# else:
+#   print("negative")
+
+# nums=6
+# if nums%2==0:
+#   print("even numbers")
+# else:
+#   print("odd")
+  
+  
+# age=18
+# if age>=18:
+#   print("You are eligible for voting")
+# else:
+#   print("You are not eligible for voting")
+
+# num1=2
+# num2=3
+# if num1>num2:
+#   print(num1)
+# else:
+#   print(num2)
+
+# marks=50
+# if marks>=50:
+#   print("pass")
+# else:
+#   print("fail")
+
+# nums=10
+# for i in range(1,nums+1):
+#   print(i)
+
+
+# for i in range(1,20):
+#   if i%2==0:
+#     print(i)
+
+# sum=0
+# for i in range(1,100):
+#   sum+=i
+# print(sum)
+
