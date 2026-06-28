c9f06c65265eaa9dbfcce4533d873af849be4f51
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-06-28 20:47:41 +0500

    update practice3

--- Week-17/practice3.py
+++ Week-17/practice3.py
@@ -25,22 +25,63 @@ print(Fore.BLUE+"Hello")
 # print(Style.BRIGHT+"hello")
 
 
-banner=pyfiglet.figlet_format("Taimour")
-print(banner)
+# banner=pyfiglet.figlet_format("Taimour")
+# print(banner)
 
 
-number1=math.floor(random.random()*10)
-number2=math.floor(random.random()*10)
+# number1=math.floor(random.random()*10)
+# number2=math.floor(random.random()*10)
 
 
-def calculator():
-  add=number1+number2
-  return add
+# def calculator():
+#   add=number1+number2
+#   return add
 
-print(calculator())
+# print(calculator())
 
-user=int(input("Enter a number to guess "))
-if user==calculator():
-  print("True you guess the right number")
-else:
-  print(f"You guess the incorrect number your number is {user} and computer number is {calculator()}")
+# user=int(input("Enter a number to guess "))
+# if user==calculator():
+#   print("True you guess the right number")
+# else:
+#   print(f"You guess the incorrect number your number is {user} and computer number is {calculator()}")
+  
+  
+  
+# with open("student.txt","w") as file:
+#   file.write("Ali\n")
+#   file.write("Ahmad\n")
+#   file.write("Sara\n")
+  
+# with open("student.txt","r") as file:
+#   reader=file.read()
+
+#   print(reader)
+  
+# user=input("Enter a user name")
+
+# with open("student.txt","a") as file:
+#   file.write(f"{user}\n")
+
+
+# with open ("marks.txt","w") as file:
+#   file.write("Ali 80\n")
+#   file.write("Ahmad 40\n")
+#   file.write("Sara 70\n")
+
+
+# total=0
+# average=0
+# count=0  
+# with open("marks.txt","r") as file:
+  
+
+#   for read in file:
+#    parts=read.split()
+#    if len(parts)==2:
+#     name=parts[0]
+#     marks=int(parts[1])
+#     total+=marks
+#     count+=1
+   
+# print("Total marks is ",total)
+# print("Average is ",total/count)
