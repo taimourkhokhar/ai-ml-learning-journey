367c68d6d88f3778ea0ec7544010cce5093e21d3
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-07-19 20:13:48 +0500

    practice 1 update

--- Week-20/practice1.py
+++ Week-20/practice1.py
@@ -47,20 +47,64 @@ merge=[]
 
 
 #increasing triplet sequence
-nums = [1,2,3,4,5]
-def increase(nums):
- first=float('inf')
- second=float('inf')
- print(first,second)
- for num in nums:
+# nums = [1,2,3,4,5]
+# def increase(nums):
+#  first=float('inf')
+#  second=float('inf')
+#  print(first,second)
+#  for num in nums:
 
-   if num<=first:
-      first=num
-      print(first)
-   elif num<=second:
-      second=num
-      print(second)
-   else:
-      return True
- return False
-print(increase(nums))
+#    if num<=first:
+#       first=num
+#       print(first)
+#    elif num<=second:
+#       second=num
+#       print(second)
+#    else:
+#       return True
+#  return False
+# print(increase(nums))
+# nums = [0,1,0,3,12]
+# left=0
+# right=0
+# while right<=len(nums)-1:
+#   if nums[left]==0:
+#     nums[left],nums[right]=nums[right],nums[left]
+#     right+=1
+#   else:
+#     left+=1
+    
+    
+# print(nums)
+
+# s = "axc"
+
+# t = "ahbgdc"
+# s_ptr=0
+# t_ptr=0
+# while t_ptr<=len(t)-1:
+#   if s[s_ptr]==t[t_ptr]:
+#     print("True")
+#     t_ptr+=1
+#     s_ptr+=1
+#   else:
+#     t_ptr+=1
+#     print("false")
+
+
+nums = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
+k = 2
+count=0
+left=0
+right=len(nums)-1
+
+while left<right:
+  if nums[left]+nums[right]==k:
+    count+=1
+    left+=1
+    right-=1
+  else:
+    left+=1
+    right-=1
+    
+print(count)
