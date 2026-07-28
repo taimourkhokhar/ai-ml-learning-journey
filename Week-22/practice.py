70890fcc6e0cd93a16a16c3a2c448fe6a9b7ee5d
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-07-28 22:00:29 +0500

    practice

---
+++ Week-22/practice.py
@@ -0,0 +1,18 @@
+# Input: word1 = "abc", word2 = "pqr"
+# Output: "apbqcr"
+
+# word1="ab"
+# word2="pqrs"
+# print(len(word1),len(word2))
+# merge=[]
+# result="" 
+# for i,j in zip(word1,word2):
+#   merge.append(i)
+#   merge.append(j)
+#   remaining=min(len(word1),len(word2))
+#   remaining=word1[remaining:]+word2[remaining:]
+#   result="".join(merge)+remaining
+# print(result)
+
+
+# print(word2[2:])
