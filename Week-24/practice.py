4f9c8cd00c0b0fa80be3aa16eae8d393964e7a42
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-08-11 20:36:26 +0500

    update practice

--- Week-24/practice.py
+++ Week-24/practice.py
@@ -17,14 +17,46 @@
 # Input: chars = ["a","a","b","b","c","c","c"]
 # Output: 6
 
-from collections import Counter
-chars=["a"]
-valuecount=Counter(chars)
-lister=list(valuecount)
-print(len(lister))
-value=2*len(lister)
-if value%2==1:
-  value=value-1
-else:
+# from collections import Counter
+# chars=["a"]
+# valuecount=Counter(chars)
+# lister=list(valuecount)
+# print(len(lister))
+# value=2*len(lister)
+# if value%2==1:
+#   value=value-1
+# else:
   
- print(value)
+#  print(value)
+
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+# nums=[0,1,0,3,12]
+# left=0
+# right=1
+
+# while right<len(nums):
+#   if nums[right]!=0:
+#     nums[left],nums[right]=nums[right],nums[left]
+#     left+=1
+#   right+=1
+  
+# print(nums)
+    
+#     Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+# s="abc"
+# t="ahbgdc"
+# leftptr=0
+# rightptr=0
+# while rightptr<=len(t)-1:
+#   if s[leftptr]==t[rightptr]:
+#     leftptr+=1
+#     rightptr+=1
+#     return True
+#   else:
+#     rightptr+=1
+#     return False
