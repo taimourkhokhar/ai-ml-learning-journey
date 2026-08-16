1ef46b04a88869d0305148d1108d6726b97703ad
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-08-17 00:19:26 +0500

    matplotlib

---
+++ Week-24/matplotlibpr.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# x=np.array([2023,2024,2025,2026])
+# y1=np.array([15,25,30,22])
+# y2=np.array([17,23,38,5])
+# y3=np.array([13,15,20,30])
+
+# plt.plot(x,y1,marker=".",markersize=30,markerfacecolor="red",markeredgecolor="red",linestyle="dashed",linewidth=3,color="red")
+
+# plt.title("class size",fontsize=20,family="Arial",fontweight="bold")
+# plt.xlabel("Year",fontsize=20)
+# plt.ylabel("student",fontsize=20)
+
+# plt.tick_params(axis="both",colors="blue")
+# plt.plot(x,y2)
+# plt.plot(x,y3)
+# plt.xticks(x)
+# plt.subplots_adjust(bottom=0.5)
+# plt.show()
+
+
+# x=[1,2,3,4,5]
+# y=[5,10,15,20,25]
+# plt.grid()
+
+
+# plt.plot(x,y)
+# plt.show()
+
+
+#bar chart
+
+# categories=["Grains","Fruit","Vegetables","Protein","Dairy","Sweets"]
+
+# values=[4,3,2,5,3,1]
+
+# plt.bar(categories,values)
+# plt.show()
