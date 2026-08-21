a5b688ca6079075f111662fafbfd0ebc410ef3c0
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-08-21 21:41:48 +0500

    practice 2

--- Week-25/practice2.py
+++ Week-25/practice2.py
@@ -6,3 +6,19 @@
 # for i in range(1,n):
 #   prefix[i]=prefix[i-1]+nums[i]
 #   print(max(prefix))
+
+# def pivotIndex(nums):
+#     total_sum = sum(nums)
+#     left_sum = 0
+
+#     for i, num in enumerate(nums):
+#         # Right sum is total_sum - left_sum - num
+#         if left_sum == (total_sum - left_sum - num):
+#             return i
+#         left_sum += num
+
+#     return -1
+
+
+# nums = [1, 7, 3, 6, 5, 6]
+# print(pivotIndex(nums))  # Output: 3
