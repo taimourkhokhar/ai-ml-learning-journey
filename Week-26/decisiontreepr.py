c09b73acc2aee9b6636f6fe52e20b44c055f0150
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-08-27 12:57:30 +0500

    decison tree practice

---
+++ Week-26/decisiontreepr.py
@@ -0,0 +1,29 @@
+# y = ["Yes", "Yes", "Yes", "No", "No"]
+
+# yes_count = 0
+# no_count = 0
+
+# for i in y:
+#     if i == "Yes":
+#         yes_count += 1
+#     else:
+#         no_count += 1
+
+# print(yes_count)
+# print(no_count)
+
+# p_yes = yes_count / len(y)
+# p_no = no_count / len(y)
+
+# print(p_yes, p_no)
+
+# gini = 1 - ((p_yes * p_yes) + (p_no * p_no))
+
+# print(gini)
+
+
+# y = ["Yes", "Yes", "Yes", "No", "No", "No"]
+# yes_count=0
+# no_count=0
+# for i in y:
+#   if i==
