b026bf0412a2038d6f1b187960441eadbd0a26fb
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-09-02 14:13:51 +0500

    practice 1

---
+++ Week-27/practice1.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+# queue=deque()
+
+# queue.append("task1")
+# queue.append("task2")
+# queue.append("task3")
+# print("queue after enqueuing",queue)
+
+# first_out=queue.popleft()
+# print("dequeued",first_out)
+# print("remaing queue",queue)
+
+# students=[1,1,1,0,0,1]
+
+# students=deque()
+# sandwiches=[1,0,0,0,1,1]
+
+# sandwiches=deque()
+# for i,j in zip(students,sandwiches):
+#   if students[i]!=sandwiches[j]:
+#     st=students.popleft()
+#     snd=sandwiches.popleft()
+#     students.append(st)
+#     sandwiches.append(snd)
+#   else:
+#     students.popleft()
+#     sandwiches.popleft()
+# print(len(students))
