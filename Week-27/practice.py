b781d494fe0cfc8700cc5adecfa0841d161503f5
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-09-03 21:43:42 +0500

    practice

--- Week-27/practice.py
+++ Week-27/practice.py
@@ -34,3 +34,28 @@
 
 #     # If the stack is empty, all brackets were validly matched
 #     return not stack
+
+# n=1
+# while n<=10:
+#   print(n)
+#   n+=1
+  
+  
+# arr=[2,4,6,8,10]
+# sum=0
+# i=0
+# while i<len(arr):
+#   sum+=arr[i]
+#   i+=1
+
+# print(sum)
+
+# arr = [3, 8, 11, 14, 20, 7, 6]
+# even_count=0
+# i=0
+# while i<len(arr):
+#   if arr[i]%2==0:
+#     even_count+=1
+#   i+=1
+  
+# print(even_count)
