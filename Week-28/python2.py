e0c3c61fc9282b5fce0a9df416741b852beaf575
unknown <taimouriftikhar2@gmail.com>
2026-09-10 17:36:27 +0500

    update

--- Week-28/python2.py
+++ Week-28/python2.py
@@ -29,30 +29,51 @@
 # for x in (car1, boat1, plane1):
 #   x.move()
 
-class Vehicle:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
+# class Vehicle:
+#   def __init__(self, brand, model):
+#     self.brand = brand
+#     self.model = model
 
-  def move(self):
-    print("Move!")
+#   def move(self):
+#     print("Move!")
 
-class Car(Vehicle):
-  pass
+# class Car(Vehicle):
+#   pass
 
-class Boat(Vehicle):
-  def move(self):
-    print("Sail!")
+# class Boat(Vehicle):
+#   def move(self):
+#     print("Sail!")
 
-class Plane(Vehicle):
-  def move(self):
-    print("Fly!")
+# class Plane(Vehicle):
+#   def move(self):
+#     print("Fly!")
 
-car1 = Car("Ford", "Mustang")       #Create a Car object
-boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
-plane1 = Plane("Boeing", "747")     #Create a Plane object
+# car1 = Car("Ford", "Mustang")       #Create a Car object
+# boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+# plane1 = Plane("Boeing", "747")     #Create a Plane object
 
-for x in (car1, boat1, plane1):
-  print(x.brand)
-  print(x.model)
-  x.move()
+# for x in (car1, boat1, plane1):
+#   print(x.brand)
+#   print(x.model)
+#   x.move()
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+  def set_age(self, age):
+    if age > 0:
+      self.__age = age
+    else:
+      print("Age must be positive")
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
