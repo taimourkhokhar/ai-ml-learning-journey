7446b0c733bd374c72f2d98b43488a659b786c0d
unknown <taimouriftikhar2@gmail.com>
2026-09-26 15:33:30 +0500

    python1 update

--- Week-30/python1.py
+++ Week-30/python1.py
@@ -13,25 +13,43 @@
 #         out+=output[i]
 # print(out)
 
-class Solution:
+# class Solution:
 
-    def romanToInt(self, s: str) -> int:
-        output = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
-        }
-        out = 0
+#     def romanToInt(self, s: str) -> int:
+#         output = {
+#             "I": 1,
+#             "V": 5,
+#             "X": 10,
+#             "L": 50,
+#             "C": 100,
+#             "D": 500,
+#             "M": 1000,
+#         }
+#         out = 0
 
-        for i in range(len(s)):
-            # If a smaller value comes before a larger value, subtract it
-            if i + 1 < len(s) and output[s[i]] < output[s[i + 1]]:
-                out -= output[s[i]]
-            else:
-                out += output[s[i]]
+#         for i in range(len(s)):
+#             # If a smaller value comes before a larger value, subtract it
+#             if i + 1 < len(s) and output[s[i]] < output[s[i + 1]]:
+#                 out -= output[s[i]]
+#             else:
+#                 out += output[s[i]]
 
-        return out
+#         return out
+
+I=1
+V=5
+X=10
+L=50
+C=100
+D=500
+M=1000
+output={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+s = "IV"
+out=0
+for i in range(len(s)):
+    if i+1<len(s) and output[s[i]] < output[s[i+1]]:
+        out-=output[s[i]]
+    else:
+        out+=output[s[i]]
+
+print(out)
